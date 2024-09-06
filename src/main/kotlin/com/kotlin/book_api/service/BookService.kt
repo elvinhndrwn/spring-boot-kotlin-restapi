cb768fd04f938f02f80c0aa73838d7d79a4163b0
elvinhndrwn <elvin.hendrawan@ist.id>
2024-09-07 01:52:06 +0700

    optimize import

--- src/main/kotlin/com/kotlin/book_api/service/BookService.kt
+++ src/main/kotlin/com/kotlin/book_api/service/BookService.kt
@@ -1,7 +1,6 @@
 package com.kotlin.book_api.service
 
 import com.kotlin.book_api.dto.BookResponse
-import com.kotlin.book_api.model.Book
 
 interface BookService {
     fun getBooks(): BookResponse
